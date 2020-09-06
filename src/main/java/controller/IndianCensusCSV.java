a1e256ee594bfe4be1f9a5b3d842deb98a275b73
soumen-gif <soumentaki1996@gmail.com>
2020-09-06 12:24:36 +0530

    Added: Welcome message before start

---
+++ src/main/java/controller/IndianCensusCSV.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class IndianCensusCSV {
+    public String state;
+
+    public int population;
+
+    public int areaInSqKm;
+
+    public int densityPerSqKm;
+
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "State='" + state + '\'' +
+                ", Population='" + population + '\'' +
+                ", AreaInSqKm='" + areaInSqKm + '\'' +
+                ", DensityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/service/CensusAnaslyser.java
@@ -0,0 +1,9 @@
+package service;
+
+public class CensusAnaslyser {
+    private static final String INDIAN_CENSUS_FILE_PATH = " ";
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to indian census analyser code");
+    }
+}
